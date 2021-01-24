30d57dc08ff49d9fc29c9ca3897a9950159265b2
Peter <Petertherock20@yahoo>
2021-01-24 20:20:58 +0000

    Update upload_docker.sh

--- upload_docker.sh
+++ upload_docker.sh
@@ -6,10 +6,14 @@
 # Step 1:
 # Create dockerpath
 # dockerpath=<your docker ID/path>
+export dockerpath=rocky20/house_prediction
 
 # Step 2:  
 # Authenticate & tag
 echo "Docker ID and Image: $dockerpath"
+docker login --username rocky20
+docker tag house_prediction $dockerpath
 
 # Step 3:
 # Push image to a docker repository
+docker push $dockerpath
