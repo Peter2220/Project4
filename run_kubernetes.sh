e9d7b9401638c995cbb5c1fe1976255583a074c6
Peter2220 <56364669+Peter2220@users.noreply.github.com>
2021-01-25 18:33:58 +0200

    Update

--- run_kubernetes.sh
+++ run_kubernetes.sh
@@ -5,18 +5,17 @@
 # Step 1:
 # This is your Docker ID/path
 # dockerpath=<>
-DOCKERUSER=rocky20
-TAG=latest
-DOCKERPATH=$DOCKERUSER/$TAG
+DOCKERPATH=rocky20/house_prediction
+TAG=prediction
 
 # Step 2
 # Run the Docker Hub container with kubernetes
-kubectl run $TAG --image=$DOCKERPATH
+kubectl run $TAG --image=$DOCKERPATH --generator=run-pod/v1
 
 # Step 3:
 # List kubernetes pods
-kubectl get pods
+kubectl get pods --all-namespaces
 
 # Step 4:
 # Forward the container port to a host
-kubectl port-forward $TAG 8000:80
+kubectl port-forward pod/$TAG 8000:80
